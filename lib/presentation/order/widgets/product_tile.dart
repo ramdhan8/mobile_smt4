05bca2766751a2316aeaf8cdbc2387dee3fa86fd
krisnatapriya <124700164+krisnatapriya@users.noreply.github.com>
2024-06-02 15:08:48 +0700

    product tile

---
+++ lib/presentation/order/widgets/product_tile.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/components/spaces.dart';
+import '../../../core/core.dart';
+import '../../home/models/product_model.dart';
+
+class ProductTile extends StatelessWidget {
+  final ProductModel data;
+  const ProductTile({super.key, required this.data});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 14.0, vertical: 10.0),
+      decoration: BoxDecoration(
+        border: Border.all(color: AppColors.stroke),
+        borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+      ),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+            child: Image.asset(
+              data.images.first,
+              width: 68.0,
+              height: 68.0,
+            ),
+          ),
+          const SpaceWidth(14.0),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                data.name,
+                style: const TextStyle(
+                  fontSize: 16,
+                ),
+                overflow: TextOverflow.ellipsis,
+              ),
+              Row(
+                children: [
+                  Text(
+                    data.priceFormat,
+                    style: const TextStyle(
+                      color: AppColors.primary,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
