54c49fe0a3ac4467919c8f2c70244c970ca397e4
krisnatapriya <124700164+krisnatapriya@users.noreply.github.com>
2024-06-02 10:24:04 +0700

    bacn account model and track record model

---
+++ lib/presentation/order/models/bank_account_model.dart
@@ -0,0 +1,11 @@
+class BankAccountModel {
+  final int code;
+  final String name;
+  final String image;
+
+  BankAccountModel({
+    required this.code,
+    required this.name,
+    required this.image,
+  });
+}
